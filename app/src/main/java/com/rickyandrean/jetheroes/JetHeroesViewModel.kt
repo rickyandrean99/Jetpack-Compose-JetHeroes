35d6191d94169f4251fa545bc72597910b197876
rickyandrean99 <84983109+rickyandrean99@users.noreply.github.com>
2022-12-12 17:09:19 +0700

    Search Feature

--- app/src/main/java/com/rickyandrean/jetheroes/JetHeroesViewModel.kt
+++ app/src/main/java/com/rickyandrean/jetheroes/JetHeroesViewModel.kt
@@ -1,5 +1,7 @@
 package com.rickyandrean.jetheroes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.rickyandrean.jetheroes.data.HeroRepository
@@ -8,12 +10,22 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 class JetHeroesViewModel(private val repository: HeroRepository) : ViewModel() {
+    private val _query = mutableStateOf("")
     private val _groupedHeroes = MutableStateFlow(
         repository.getHeroes()
             .sortedBy { it.name }
             .groupBy { it.name[0] }
     )
+
     val groupedHeroes: StateFlow<Map<Char, List<Hero>>> get() = _groupedHeroes
+    val query: State<String> get() = _query
+
+    fun search(newQuery: String) {
+        _query.value = newQuery
+        _groupedHeroes.value = repository.searchHeroes(_query.value)
+            .sortedBy { it.name }
+            .groupBy { it.name[0] }
+    }
 }
 
 class ViewModelFactory(private val repository: HeroRepository) :
